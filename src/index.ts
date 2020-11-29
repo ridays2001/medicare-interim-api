4e3d4e02c413c52052670c4ced0ff2fd7a904166
Riday <ridayshah2001@gmail.com>
2020-11-29 18:16:08 +0530

    Chore: linked etc router

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import logger from 'morgan';
 
 import AuthRouter from './routers/auth';
+import EtcRouter from './routers/etc';
 
 import express from 'express';
 const app = express();
@@ -14,8 +15,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (_req, res) => res.end('Hello World!'));
+app.get('/', (_, res) => res.end('Hello World!'));
 app.use('/auth', AuthRouter);
+app.use('/etc', EtcRouter);
 
 const port = Number(process.env.PORT as string) || 80;
 app.listen(port, () => console.log(`Listening on http://localhost${port === 80 ? '' : port}/`));
